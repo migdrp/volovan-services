4f0428df3b5f789193896aab6efe4b91bcac890e
Miguel Rebolledo <mig_drp@hotmail.com>
2022-12-08 03:07:54 -0600

    Dev cookies change in prod

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -60,8 +60,8 @@ router.get('/refresh', async (req: Request, res: Response, next: NextFunction) =
 
     const { accessToken, refreshToken, userData } = await refreshUserToken(token);
 
-    res.cookie("accessToken", accessToken, { httpOnly: true, maxAge: parseInt(process.env.JWT_ACCESS_TIME) * 1000 });
-    res.cookie("refreshToken", refreshToken, { httpOnly: true, maxAge: parseInt(process.env.JWT_REFRESH_TIME) * 1000 });
+    res.cookie("accessToken", accessToken, { httpOnly: true, maxAge: parseInt(process.env.JWT_ACCESS_TIME) * 1000, sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', secure: process.env.NODE_ENV === 'production' });
+    res.cookie("refreshToken", refreshToken, { httpOnly: true, maxAge: parseInt(process.env.JWT_REFRESH_TIME) * 1000, sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', secure: process.env.NODE_ENV === 'production' });
     req.headers.authorization = `Bearer ${accessToken}`;
 
     res.json({
@@ -105,8 +105,8 @@ router.get('/logout', async (req: Request, res: Response, next: NextFunction) =>
     if (!refreshToken && !accessToken && !tokenFromHeader) throw new Error('Not user session found.');
 
     userData = await destroyUserToken(refreshToken);
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken", { sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', secure: process.env.NODE_ENV === 'production', httpOnly: true });
+    res.clearCookie("refreshToken", { sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', secure: process.env.NODE_ENV === 'production', httpOnly: true });
     res.json({
       status: 'success',
       message: 'User session closed.',
